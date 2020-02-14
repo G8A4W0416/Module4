0a7a9e0eef58f28a44bfd2c8811e843dabbb5d40
G8A4W0416 <wilhelmg1@gmail.com>
2020-02-14 04:12:05 -0600

    Second test set added with failing tests. Cleaned up first test set.

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -2,7 +2,7 @@ SALES_TAX = .06
 
 
 def calculate_price(price, cash_coupon, percent_coupon):
-    if price <= 5 and cash_coupon == 5 or price <= 10 and cash_coupon == 10:
+    if price <= 5 and cash_coupon == 5 or price < 10 and cash_coupon == 10:
         total_price = 5.95
     else:
         deduct_cash_coupon = price - cash_coupon
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -3,25 +3,33 @@ from store import coupon_calculations
 
 
 class MyTestCase(unittest.TestCase):
-    def test_price_under_ten_1(self):
-        self.assertEqual(8.49, coupon_calculations.calculate_price(8.00, 5.00, .10))
+    def test_price_under_ten(self):
+        with self.subTest():
+            self.assertEqual(8.49, coupon_calculations.calculate_price(8.00, 5.00, .10))
+        with self.subTest():
+            self.assertEqual(5.95, coupon_calculations.calculate_price(5.00, 5.00, .15))
+        with self.subTest():
+            self.assertEqual(5.95, coupon_calculations.calculate_price(1.00, 5.00, .20))
+        with self.subTest():
+            self.assertEqual(5.95, coupon_calculations.calculate_price(9.99, 10.00, .10))
+        with self.subTest():
+            self.assertEqual(5.95, coupon_calculations.calculate_price(0.01, 10.00, .15))
+        with self.subTest():
+            self.assertEqual(5.95, coupon_calculations.calculate_price(5.00, 10.00, .20))
 
-    # The following tests default to the 5.95 shipping cost since the item price will be zero when it matches or is
-    # less than the cash-off coupon amount.
-    def test_price_under_ten_2(self):
-        self.assertEqual(5.95, coupon_calculations.calculate_price(5.00, 5.00, .15))
-
-    def test_price_under_ten_3(self):
-        self.assertEqual(5.95, coupon_calculations.calculate_price(1.00, 5.00, .20))
-
-    def test_price_under_ten_4(self):
-        self.assertEqual(5.95, coupon_calculations.calculate_price(9.99, 10.00, .10))
-
-    def test_price_under_ten_5(self):
-        self.assertEqual(5.95, coupon_calculations.calculate_price(0.01, 10.00, .15))
-
-    def test_price_under_ten_6(self):
-        self.assertEqual(5.95, coupon_calculations.calculate_price(5.00, 10.00, .20))
+    def test_price_under_between_ten_thirty(self):
+        with self.subTest():
+            self.assertEqual(12.72, coupon_calculations.calculate_price(10.00, 5.00, .10))
+        with self.subTest():
+            self.assertEqual(21.47, coupon_calculations.calculate_price(20.00, 5.00, .15))
+        with self.subTest():
+            self.assertEqual(29.14, coupon_calculations.calculate_price(29.99, 5.00, .20))
+        with self.subTest():
+            self.assertEqual(7.95, coupon_calculations.calculate_price(10.00, 10.00, .10))
+        with self.subTest():
+            self.assertEqual(16.96, coupon_calculations.calculate_price(20.00, 10.00, .15))
+        with self.subTest():
+            self.assertEqual(24.90, coupon_calculations.calculate_price(29.99, 10.00, .20))
 
 
 if __name__ == '__main__':
